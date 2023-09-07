ac196433da5c1ac715b84def819b238bd31f6ab6
Anuragkumarsah <asah2307@gmail.com>
2023-09-07 12:12:08 +0530

    switched to own deployment of consumet api

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const ANIME_URL = "https://consumet-api-anuragkumarsah.vercel.app/meta/anilist";
 export const ENIME_URL = "https://api.enime.moe";
-export const ANIME_NEWS_URL = "https://api.consumet.org/news/ann";
+export const ANIME_NEWS_URL = "https://consumet-api-anuragkumarsah.vercel.app/news/ann";
 
 export const BASE_URL = `${process.env.NODE_ENV === "production" ? "https" : "http"}://${process.env.VERCEL_URL}/api`;
