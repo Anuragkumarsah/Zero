4749be097901e04391ee4373a8ae2631c02f3483
Anuragkumarsah <asah2307@gmail.com>
2023-09-07 11:59:19 +0530

    removed popular

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
   const { getRecent, getPopular, getTrending, getNewsFeed } = useAnime();
   
   const recentAnime : { results: AnimeData[] } = await getRecent();
-  const popularAnime : { results: PopularAnimeData[] } = await getPopular();
+  // const popularAnime : { results: PopularAnimeData[] } = await getPopular();
   // console.log(recentAnime);
   // const trendingAnime : {results: PopularAnimeData[]} = await getTrending();
   
@@ -29,7 +29,7 @@ export default async function Home() {
         <div className={styles.main_body}>
           <div className={styles.anime_container}>
           <AnimeContainer container_title="Latest Episodes" recentAnime={recentAnime.results} popularAnime={null}/>
-          <AnimeContainer container_title="Popular Episodes" popularAnime={popularAnime.results} recentAnime={null}/>
+          {/* <AnimeContainer container_title="Popular Episodes" popularAnime={popularAnime.results} recentAnime={null}/> */}
           </div>
           <NewsFeeds newsFeed={newsFeed.slice(0,10)}/>
         </div>
