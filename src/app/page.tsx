8d97e863392ee14f510774ffbf92ac08a06d22f6
Anuragkumarsah <asah2307@gmail.com>
2023-09-07 12:00:56 +0530

    removed recent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { getRecent, getPopular, getTrending, getNewsFeed } = useAnime();
   
-  const recentAnime : { results: AnimeData[] } = await getRecent();
+  // const recentAnime : { results: AnimeData[] } = await getRecent();
   // const popularAnime : { results: PopularAnimeData[] } = await getPopular();
   // console.log(recentAnime);
   // const trendingAnime : {results: PopularAnimeData[]} = await getTrending();
@@ -28,7 +28,7 @@ export default async function Home() {
         {/* <Carousel topAnime={trendingAnime.results}/> */}
         <div className={styles.main_body}>
           <div className={styles.anime_container}>
-          <AnimeContainer container_title="Latest Episodes" recentAnime={recentAnime.results} popularAnime={null}/>
+          {/* <AnimeContainer container_title="Latest Episodes" recentAnime={recentAnime.results} popularAnime={null}/> */}
           {/* <AnimeContainer container_title="Popular Episodes" popularAnime={popularAnime.results} recentAnime={null}/> */}
           </div>
           <NewsFeeds newsFeed={newsFeed.slice(0,10)}/>
