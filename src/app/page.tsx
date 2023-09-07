3cb96d1ec3df13c68bb18a29a3218ad0811404bb
Anuragkumarsah <asah2307@gmail.com>
2023-09-07 12:05:11 +0530

    added carousel recent and popular back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,10 @@ export default async function Home() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { getRecent, getPopular, getTrending, getNewsFeed } = useAnime();
   
-  // const recentAnime : { results: AnimeData[] } = await getRecent();
-  // const popularAnime : { results: PopularAnimeData[] } = await getPopular();
+  const recentAnime : { results: AnimeData[] } = await getRecent();
+  const popularAnime : { results: PopularAnimeData[] } = await getPopular();
   // console.log(recentAnime);
-  // const trendingAnime : {results: PopularAnimeData[]} = await getTrending();
+  const trendingAnime : {results: PopularAnimeData[]} = await getTrending();
   
   // const newsFeed : NewsFeed[] = await getNewsFeed();
 
@@ -25,11 +25,11 @@ export default async function Home() {
 
   return (
     <div className={styles.app}>
-        {/* <Carousel topAnime={trendingAnime.results}/> */}
+        <Carousel topAnime={trendingAnime.results}/>
         <div className={styles.main_body}>
           <div className={styles.anime_container}>
-          {/* <AnimeContainer container_title="Latest Episodes" recentAnime={recentAnime.results} popularAnime={null}/> */}
-          {/* <AnimeContainer container_title="Popular Episodes" popularAnime={popularAnime.results} recentAnime={null}/> */}
+          <AnimeContainer container_title="Latest Episodes" recentAnime={recentAnime.results} popularAnime={null}/>
+          <AnimeContainer container_title="Popular Episodes" popularAnime={popularAnime.results} recentAnime={null}/>
           </div>
           {/* <NewsFeeds newsFeed={newsFeed.slice(0,10)}/> */}
         </div>
